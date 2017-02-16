207b658ef32a43665b057c6d91642b962575ae9f
Nick Van Vynckt <nick.vanvynckt@hotmail.com>
2017-02-16 15:58:48 +0100

    [FEATURE] Working on the tests

---
+++ test/companyTest.js
@@ -0,0 +1,10 @@
+'use strict';
+
+var supertest = require('supertest');
+var should = require('should');
+
+var server = supertest.agent('http://localhost:3000');
+
+describe('GET', function() {
+    it()
+});
