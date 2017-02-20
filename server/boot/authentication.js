a11a3e062acd093554219c2b5c470935cad0896e
nickvanvynckt <nick.vanvynckt@hotmail.com>
2017-02-20 10:52:43 +0100

    Forgot to delete a line in the boot-script

--- server/boot/authentication.js
+++ server/boot/authentication.js
@@ -1,7 +1,5 @@
 'use strict';
 
-var PassportConfigurator = require('loopback-component-passport').PassportConfigurator;
-
 module.exports = function enableAuthentication(server) {
   // enable authentication
   server.enableAuth();
