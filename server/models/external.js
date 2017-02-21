e95a2aacafa4e24781bc29852dd90b516180a701
nickvanvynckt <nick.vanvynckt@hotmail.com>
2017-02-21 09:38:56 +0100

    Added the functionality to the external to get information with a google search. Some names do give some problems

--- server/models/external.js
+++ server/models/external.js
@@ -1,5 +1,44 @@
 'use strict';
 
-module.exports = function(External) {
+var GoogleSearch = require('google-search');
+var googleSearch = new GoogleSearch({
+    key: 'AIzaSyAWqi1qfUyGPOTcmhE0gu-5Ik8hz8Ncjr8',
+    cx: '002086392339423832635:4taepnplwtq'
+});
 
+module.exports = function(External) {
+    External.getInformation = function(name, cb) {
+      var resp;
+      googleSearch.build({
+            q: name,
+            lr: 'lang_nl'
+      }, function(error, response) {
+          var found = false;
+          var index = 0;
+          while(!found && index < 10) {
+            if(response.items[index].pagemap.hcard[0].fn == name){
+              resp = response.items[index].pagemap.hcard[0];
+              found = true;
+            } else {
+              console.log("Incrementing the index from: "+index);
+              index++;
+            }
+          }
+          cb(null, resp);
+        });
+    };
+    External.remoteMethod('getInformation', {
+        http: {
+            path: '/getInformation',
+            verb: 'get'
+        },
+        accepts: {
+          arg: 'name',
+          type: 'string'
+        },
+        returns: {
+            arg: 'information',
+            type: 'Object'
+        }
+    });
 };
