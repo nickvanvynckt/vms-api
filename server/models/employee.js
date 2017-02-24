40c6a71c0fd1941ea83899ac736675c3f344c608
Bert Vandormael <bert.vandormael@gmail.com>
2017-02-24 10:03:24 +0100

    [FEATURE] Google access tokens automatic refresh

--- server/models/employee.js
+++ server/models/employee.js
@@ -1,74 +1,90 @@
 'use strict';
 
 var GoogleCalendar = require('google-calendar');
+var refresh = require('passport-oauth2-refresh');
 
-module.exports = function(Employee) {
+module.exports = function (Employee) {
 
-    Employee.getMeetings = function(name, cb) {
+    function refreshToken(UserIdentity, token, id, cb) {
+        refresh.requestNewAccessToken('google', token, function (err, accessToken, refreshToken) {
+            if (refreshToken === null || refreshToken === undefined) {
+                refreshToken = token;
+            }
+            UserIdentity.upsertWithWhere({ and: [{ employeeId: id }, { provider: 'google-login' }] }, { credentials: { accessToken: accessToken, refreshToken: refreshToken } }, function (err, obj) {
+                console.log(err);
+                console.log(obj);
+                cb(accessToken);
+            });
+        });
+    }
+
+    Employee.getMeetings = function (name, cb) {
         var resp;
         const app = this.app;
         const UserIdentity = this.app.models.UserIdentity;
 
-        Employee.findOne({where: {username:name} }, function(err, emp) {
+        Employee.findOne({ where: { username: name } }, function (err, emp) {
             if (err !== null) {
                 cb(err);
             } else if (emp !== null) {
                 var id = emp.id;
                 var calendarIds = emp.calendars;
 
-                if(calendarIds.length === 0) {
+                if (calendarIds.length === 0) {
                     cb("No calendars selected.");
                 } else {
-                    UserIdentity.findOne({where:{and:[{employeeId:id}, {provider:'google-login'}]}}, function(err, ui) {
-                    var accessToken = ui.credentials.accessToken;
-                    var errs = [];
-                    var meetings = [];
-                    var loopDone = 0;
+                    UserIdentity.findOne({ where: { and: [{ employeeId: id }, { provider: 'google-login' }] } }, function (err, ui) {
+                        refreshToken(UserIdentity, ui.credentials.refreshToken, id, function (accessToken) {
+                            var errs = [];
+                            var meetings = [];
+                            var loopDone = 0;
 
-                    for(var i = 0; i < calendarIds.length; i++) {
-                        getMeetingsById(app, accessToken, calendarIds[i], function(data) {
-                            if(data.err !== null) {
-                                errs.push(data.err);
-                            } else {
-                                meetings = meetings.concat(data.list);
-                            }
-                            loopDone++;
-                            if(loopDone === calendarIds.length) {
-                                if(errs.length > 0) {
-                                    cb(errs);
-                                } else {
-                                    cb(null, meetings);
-                                }
+                            for (var i = 0; i < calendarIds.length; i++) {
+                                getMeetingsById(app, accessToken, calendarIds[i], function (data) {
+                                    if (data.err !== null) {
+                                        errs.push(data.err);
+                                    } else {
+                                        meetings = meetings.concat(data.list);
+                                    }
+                                    loopDone++;
+                                    if (loopDone === calendarIds.length) {
+                                        if (errs.length > 0) {
+                                            cb(errs);
+                                        } else {
+                                            cb(null, meetings);
+                                        }
+                                    }
+                                });
                             }
                         });
-                    }
-                });
+                    });
                 }
             } else {
                 cb("Unable to find user in database.");
             }
-        });        
+        });
     }
 
-    Employee.getMeetingsByCalendarId = function(name, calendarId, cb) {
+    Employee.getMeetingsByCalendarId = function (name, calendarId, cb) {
         var resp;
         const app = this.app;
         const UserIdentity = this.app.models.UserIdentity;
 
-        Employee.findOne({where: {username:name} }, function(err, emp) {
+        Employee.findOne({ where: { username: name } }, function (err, emp) {
             if (err !== null) {
                 cb(err);
             } else if (emp !== null) {
                 var id = emp.id;
 
-                UserIdentity.findOne({where:{and:[{employeeId:id}, {provider:'google-login'}]}}, function(err, ui) {
-                    var accessToken = ui.credentials.accessToken;
-                    getMeetingsById(app, accessToken, calendarId, function(data){
-                        if(data.err !== null) {
-                            cb(data.err);
-                        } else {
-                            cb(null, data.list);
-                        }
+                UserIdentity.findOne({ where: { and: [{ employeeId: id }, { provider: 'google-login' }] } }, function (err, ui) {
+                    refreshToken(UserIdentity, ui.credentials.refreshToken, id, function (accessToken) {
+                        getMeetingsById(app, accessToken, calendarId, function (data) {
+                            if (data.err !== null) {
+                                cb(data.err);
+                            } else {
+                                cb(null, data.list);
+                            }
+                        });
                     });
                 });
             } else {
@@ -77,27 +93,28 @@ module.exports = function(Employee) {
         });
     }
 
-    Employee.getCalendars = function(name, cb) {
+    Employee.getCalendars = function (name, cb) {
         var resp;
         const UserIdentity = this.app.models.UserIdentity;
 
-        Employee.findOne({where: {username:name} }, function(err, emp) {
+        Employee.findOne({ where: { username: name } }, function (err, emp) {
             if (err !== null) {
                 cb(err);
             } else if (emp !== null) {
                 var id = emp.id;
 
-                UserIdentity.findOne({where:{and:[{employeeId:id}, {provider:'google-login'}]}}, function(err, ui) {
-                    var accessToken = ui.credentials.accessToken;
+                UserIdentity.findOne({ where: { and: [{ employeeId: id }, { provider: 'google-login' }] } }, function (err, ui) {
 
-                    var googleCalendar = new GoogleCalendar(accessToken);
+                    refreshToken(UserIdentity, ui.credentials.refreshToken, id, function (accessToken) {
+                        var googleCalendar = new GoogleCalendar(accessToken);
 
-                    googleCalendar.calendarList.list(function(err, calendarList) {
-                        if(err !== null) {
-                            cb(err);
-                        } else {
-                            cb(null, calendarList.items);
-                        }
+                        googleCalendar.calendarList.list(function (err, calendarList) {
+                            if (err !== null) {
+                                cb(err);
+                            } else {
+                                cb(null, calendarList.items);
+                            }
+                        });
                     });
                 });
             } else {
@@ -108,18 +125,18 @@ module.exports = function(Employee) {
 
     function getMeetingsById(app, token, id, cb) {
         var googleCalendar = new GoogleCalendar(token);
-        var data = {err: "Unable to get meetings for calendar with id " + id, list:null};
+        var data = { err: "Unable to get meetings for calendar with id " + id, list: null };
         var oneWeekAgo = new Date();
         oneWeekAgo.setDate(oneWeekAgo.getDate() - 7);
         var nextTwoWeeks = new Date();
         nextTwoWeeks.setDate(nextTwoWeeks.getDate() + 14);
 
-        googleCalendar.events.list(id, {timeMin:oneWeekAgo.toISOString(), timeMax:nextTwoWeeks.toISOString()}, function(err, eventsList) {
-            if(err !== null) {
-                data = {err:err, list:null};
+        googleCalendar.events.list(id, { timeMin: oneWeekAgo.toISOString(), timeMax: nextTwoWeeks.toISOString() }, function (err, eventsList) {
+            if (err !== null) {
+                data = { err: err, list: null };
                 cb(data);
-            } else if(eventsList !== null) {
-                cb({err:null, list:eventsList.items});
+            } else if (eventsList !== null) {
+                cb({ err: null, list: eventsList.items });
                 //addToMeetings(app, eventsList, cb);
             }
         });
@@ -131,21 +148,21 @@ module.exports = function(Employee) {
         var errs = [];
         var returnList = [];
 
-        for(var i = 0; i < list.length; i++) {
+        for (var i = 0; i < list.length; i++) {
             Meeting.upsert({
-                room:list[i]
-            }, function(err, obj) {
-                if(err !== null) {
+                room: list[i]
+            }, function (err, obj) {
+                if (err !== null) {
                     errs.push(err);
                 } else {
                     returnList.push(obj);
                 }
-                if(loopDone === list.length) {
-                    if(errs.length > 0) {
-                        cb({err:errs, list:null});
+                if (loopDone === list.length) {
+                    if (errs.length > 0) {
+                        cb({ err: errs, list: null });
                     } else {
-                        cb({err:null, list:list});
-                    } 
+                        cb({ err: null, list: list });
+                    }
                 }
             });
         }
@@ -157,8 +174,8 @@ module.exports = function(Employee) {
             verb: 'get'
         },
         accepts: {
-          arg: 'username',
-          type: 'string'
+            arg: 'username',
+            type: 'string'
         },
         returns: {
             arg: 'meetings',
@@ -193,8 +210,8 @@ module.exports = function(Employee) {
             verb: 'get'
         },
         accepts: {
-          arg: 'username',
-          type: 'string'
+            arg: 'username',
+            type: 'string'
         },
         returns: {
             arg: 'calendars',
