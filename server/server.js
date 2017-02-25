6ec4ef53341bbecd025c1319337042f6447e2e00
nickvanvynckt <nick.vanvynckt@hotmail.com>
2017-02-25 14:29:02 +0100

    [PATCH] Changed the configuration of the Linkedin login

--- server/server.js
+++ server/server.js
@@ -6,56 +6,64 @@ var bodyParser = require('body-parser');
 var cookieParser = require('cookie-parser');
 var session = require('express-session');
 var flash = require('express-flash');
+var cors = require('cors');
+var passport = require('passport');
+var refresh = require('passport-oauth2-refresh');
+var GoogleStrategy = require('passport-google-oauth2').Strategy;
+var LinkedinStrategy = require('passport-linkedin-oauth2').Strategy;
+var loopbackPassport = require('loopback-component-passport-c');
 
 var app = module.exports = loopback();
 
 app.set('cookieSecret', 'keyboard cat');
 
-// Passport configurators..
-var loopbackPassport = require('loopback-component-passport-c');
-var PassportConfigurator = loopbackPassport.PassportConfigurator;
-var passportConfigurator = new PassportConfigurator(app);
+app.use(function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
 
-//Passport config through providers.json
-var config = {};
-try {
-    config = require('../providers.json');
-} catch (err) {
-    console.trace(err);
-    process.exit(1); // fatal
-}
+    if ('OPTIONS' == req.method) {
+        res.send(200);
+    } else {
+        next();
+    }
+});
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.middleware('session:before', cookieParser(app.get('cookieSecret')));
-app.middleware('session', session({
-    secret: 'keyboard cat',
-    saveUninitialized: true,
-    resave: false,
-    signed: true
-}));
-passportConfigurator.init();
+app.middleware('session', session({secret: 'keyboard cat', saveUninitialized: true, resave: false, signed: true}));
 
 //Body parser config
 app.middleware('parse', bodyParser.json());
-app.middleware('parse', bodyParser.urlencoded({
-    extended: true
-}));
+app.middleware('parse', bodyParser.urlencoded({extended: true}));
 // The access token is only available after boot
-app.middleware('auth', loopback.token({
-    model: app.models.AccessToken
-}));
+app.middleware('auth', loopback.token({model: app.models.AccessToken}));
+
+// Passport configurators..
+var PassportConfigurator = loopbackPassport.PassportConfigurator;
+var passportConfigurator = new PassportConfigurator(app);
+
+passport.serializeUser(function(user, done) {
+  done(null, user);
+});
+
+passport.deserializeUser(function(user, done) {
+  done(null, user);
+});
 
 // Bootstrap the application, configure models, datasources and middleware.
 // Sub-apps like REST API are mounted via boot scripts.
-boot(app, __dirname, function (err) {
-    if (err) throw err;
+boot(app, __dirname, function(err) {
+    if (err)
+        throw err;
 
     // start the server if `$ node server.js`
     if (require.main === module)
         app.start();
-});
-
-// We need flash messages to see passport errors
-app.use(flash());
+    }
+);
 
 passportConfigurator.setupModels({
     userModel: app.models.employee,
@@ -63,55 +71,112 @@ passportConfigurator.setupModels({
     userCredentialModel: app.models.userCredential
 });
 
-for (var s in config) {
-    var c = config[s];
-    c.session = c.session !== false;
-    passportConfigurator.configureProvider(s, c);
-}
-
-var passport = require('passport');
-var refresh = require('passport-oauth2-refresh');
-var GoogleStrategy = require('passport-google-oauth2').Strategy;
+// We need flash messages to see passport errors
+app.use(flash());
 
 var googleStrategy = new GoogleStrategy({
     clientID: "258576207390-v3eeisflali8goj9dp4qrq9q0p98rpfg.apps.googleusercontent.com",
     clientSecret: "G0Vu58pzXxN-Yggbcu8O3BvO",
-    callbackURL: "http://localhost:3000/auth/google/callback",
+    callbackURL: "http://localhost:4000/auth/google/callback",
     passReqToCallback: true
-},
-    function (request, accessToken, refreshToken, profile, done) {
-        app.models.employee.findOne({ where: { and: [{ fname: profile.name.givenName }, { lname: profile.name.familyName }] } }, function (err, emp) {
-            var id = emp.id;
-            profile.externalId = profile.id;
-            delete profile["id"];
-            profile.employeeId = id;
-            profile.credentials = {accessToken:accessToken, refreshToken:refreshToken};
-            profile.provider = "google-login";
-            app.models.UserIdentity.upsertWithWhere({externalId:profile.externalId}, profile, function(err, obj) {
-                return done(err, obj);
-            });
+}, function(request, accessToken, refreshToken, profile, done) {
+    app.models.employee.findOne({
+        where: {
+            and: [
+                {
+                    fname: profile.name.givenName
+                }, {
+                    lname: profile.name.familyName
+                }
+            ]
+        }
+    }, function(err, emp) {
+        var id = emp.id;
+        profile.externalId = profile.id;
+        delete profile["id"];
+        profile.employeeId = id;
+        profile.credentials = {
+            accessToken: accessToken,
+            refreshToken: refreshToken
+        };
+        profile.provider = "google-login";
+        app.models.UserIdentity.upsertWithWhere({
+            externalId: profile.externalId
+        }, profile, function(err, obj) {
+            return done(err, obj);
         });
-    }
-);
+    });
+});
+
+var linkedInStrategy = new LinkedinStrategy({
+    clientID: "86q32krgcwud98",
+    clientSecret: "jHTjkR8pKTMWAgZt",
+    callbackURL: "http://localhost:4000/auth/linkedin/callback"
+}, function(request, token, tokenSecret, profile, done){
+    console.log(token);
+    app.models.employee.findOne({
+        where: {
+            and: [
+                {
+                    fname: profile.name.givenName
+                }, {
+                    lname: profile.name.familyName
+                }
+            ]
+        }
+    }, function(err, emp) {
+        var id = emp.id;
+        delete profile["id"];
+        profile.employeeId = id;
+        profile.provider = "linkedin-login";
+        profile.credentials = {
+            accessToken: tokenSecret
+        }
+        app.models.UserIdentity.upsertWithWhere({
+            externalId: profile.externalId
+        }, profile, function(err, obj) {
+            return done(err, obj);
+        });
+    });
+});
 
 passport.use(googleStrategy);
+passport.use(linkedInStrategy);
 refresh.use(googleStrategy);
 
+app.get('/auth/google', passport.authenticate('google', {
+    scope: [
+        "https://www.googleapis.com/auth/userinfo.email", "https://www.googleapis.com/auth/userinfo.profile", "https://www.googleapis.com/auth/calendar"
+    ],
+    accessType: "offline",
+    prompt: "consent",
+    action: "page",
+    json: true
+}));
 
-app.get('/auth/google',
-    passport.authenticate('google', {
-        scope: ["https://www.googleapis.com/auth/userinfo.email", "https://www.googleapis.com/auth/userinfo.profile", "https://www.googleapis.com/auth/calendar"],
-        accessType: "offline",
-        prompt: "consent",
-        json: true
-    }));
+app.get('/auth/google/callback', passport.authenticate('google', {
+    successRedirect: '/',
+    failureRedirect: '/login',
+    responseType: "token",
+    failureFlash: true
+}));
 
-app.get('/auth/google/callback',
-    passport.authenticate('google', { successRedirect: '/', failureRedirect: '/login', failureFlash: true }));
+app.get('/auth/linkedin', passport.authenticate('linkedin', {
+    "scope": ["r_basicprofile", "r_emailaddress"],
+    "state": true,
+    "json": true
+}));
 
-app.start = function () {
+app.get('/auth/linkedin/callback', passport.authenticate('linkedin', {
+    successRedirect: '/',
+    failureRedirect: '/login',
+    responseType: "token",
+    failureFlash: true
+}));
+
+app.start = function() {
     // start the web server
-    return app.listen(function () {
+    return app.listen(function() {
         app.emit('started');
         var baseUrl = app.get('url').replace(/\/$/, '');
         console.log('Web server listening at: %s', baseUrl);
