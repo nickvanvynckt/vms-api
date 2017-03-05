8ebe0d31fab92c44ceacc8b48cc9848de83cd9b0
Bert Vandormael <bert.vandormael@gmail.com>
2017-03-05 22:07:37 +0100

    [FEATURE] Get all meetings in database with /meetings/all

--- server/models/meeting.js
+++ server/models/meeting.js
@@ -2,4 +2,26 @@
 
 module.exports = function(Meeting) {
 
+    Meeting.getAll = function(cb) {
+        Meeting.find({ include: ['meetees', 'externals'] }, function(err, list) {
+            if (err !== null) {
+                cb(err);
+            } else {
+                cb(null, list);
+            }
+        });
+    }
+
+    Meeting.remoteMethod('getAll', {
+        http: {
+            path: '/all',
+            verb: 'get'
+        },
+        returns: {
+            arg: 'meetings',
+            type: 'array'
+        },
+        description: 'Returns all meetings in the database.'
+    });
+
 };
